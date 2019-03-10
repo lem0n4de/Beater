516ae82c5aac94e99a846c591efdae13d9d25c6d
Yunus Emre ÖRCÜN <yunusemreorcun@protonmail.com>
2019-03-10 21:19:00 +0300

    Added ActivityTest annotation to test

--- app/src/androidTest/kotlin/com/lem0n/beater/StartAppTest.kt
+++ app/src/androidTest/kotlin/com/lem0n/beater/StartAppTest.kt
@@ -1,9 +1,11 @@
 package com.lem0n.beater
 
 import com.agoda.kakao.screen.Screen
+import de.mannodermaus.junit5.ActivityTest
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
+@ActivityTest(MainActivity::class)
 class StartAppTest {
     @Test
     @DisplayName(
@@ -20,13 +22,13 @@ class StartAppTest {
             }
 
             dialogButton1 {
-                matches { withText(R.string.role_server) }
+                matches { withText(R.string.role_client) }
                 matches { isClickable() }
                 matches { isDisplayed() }
             }
 
             dialogButton2 {
-                matches { withText(R.string.role_client) }
+                matches { withText(R.string.role_server) }
                 matches { isClickable() }
                 matches { isDisplayed() }
             }
