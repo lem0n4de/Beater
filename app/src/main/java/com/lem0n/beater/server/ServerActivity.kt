3b0f99ed004aa2cedcf7aafbb03d57e34c5ae617
Yunus Emre ÖRCÜN <yunusemreorcun@protonmail.com>
2019-03-15 20:08:29 +0300

    Fix : Unresolved reference of R in ServerActivity

--- app/src/main/java/com/lem0n/beater/server/ServerActivity.kt
+++ app/src/main/java/com/lem0n/beater/server/ServerActivity.kt
@@ -2,6 +2,7 @@ package com.lem0n.beater.server
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.lem0n.beater.R
 
 class ServerActivity : AppCompatActivity() {
 
