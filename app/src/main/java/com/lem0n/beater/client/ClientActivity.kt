0b637ae52fe4eb282cf13810049b63dce5dd0b7f
Yunus Emre ÖRCÜN <yunusemreorcun@protonmail.com>
2019-03-15 16:27:43 +0300

    Client Activity added.

---
+++ app/src/main/java/com/lem0n/beater/client/ClientActivity.kt
@@ -0,0 +1,13 @@
+package com.lem0n.beater.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lem0n.beater.R
+
+class ClientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client)
+    }
+}
