b578f7a25427ce03313b3117ebc8b5d3b78bac2b
Yunus Emre ÖRCÜN <yunusemreorcun@protonmail.com>
2019-03-15 23:55:33 +0300

    ClientService added.

---
+++ app/src/main/java/com/lem0n/beater/client/ClientService.kt
@@ -0,0 +1,12 @@
+package com.lem0n.beater.client
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ClientService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
