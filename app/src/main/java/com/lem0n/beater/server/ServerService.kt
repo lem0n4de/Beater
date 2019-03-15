8fc192a2391f784a323a508e67e66c7a14d40d1d
Yunus Emre ÖRCÜN <yunusemreorcun@protonmail.com>
2019-03-15 23:54:33 +0300

    Feat : ServerService added.

---
+++ app/src/main/java/com/lem0n/beater/server/ServerService.kt
@@ -0,0 +1,12 @@
+package com.lem0n.beater.server
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ServerService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
