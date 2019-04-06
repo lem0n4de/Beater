98a6b20a4d0cd12c841b35bf2bdbbb20c1b4c11a
Yunus Emre ÖRCÜN <yunusemreorcun@protonmail.com>
2019-04-06 13:38:41 +0300

    Bug : Added ClientReceiver to module.

--- common/src/main/java/com/lem0n/common/KoinModule.kt
+++ common/src/main/java/com/lem0n/common/KoinModule.kt
@@ -2,11 +2,13 @@ package com.lem0n.common
 
 import com.lem0n.common.EventBus.EventBus
 import com.lem0n.common.EventBus.IEventBus
+import com.lem0n.common.Receivers.ClientReceiver
 import com.lem0n.common.Receivers.ServerReceiver
 import org.koin.dsl.module
 
 
 val commonModule = module {
     single<ServerReceiver> { ServerReceiver() }
+    single<ClientReceiver> { ClientReceiver() }
     single<IEventBus> { EventBus }
 }
