8c123e44910aac0b09f7e32813419d097fd25b1f
Yunus Emre ÖRCÜN <yunusemreorcun@protonmail.com>
2019-04-07 23:16:23 +0300

    EventBus publisher turned into RxRelay

--- common/src/main/java/com/lem0n/common/EventBus/EventBus.kt
+++ common/src/main/java/com/lem0n/common/EventBus/EventBus.kt
@@ -1,13 +1,14 @@
 package com.lem0n.common.EventBus
 
+import com.jakewharton.rxrelay2.PublishRelay
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 
 object EventBus : IEventBus {
-    private val publisher = PublishSubject.create<Any>()
+    private val publisher = PublishRelay.create<Event>()
 
     override fun publish(event: Event) {
-        publisher.onNext(event)
+        publisher.accept(event)
     }
 
     override fun <T : Event> listen(eventType: Class<T>): Observable<T> = publisher.ofType(eventType)
